29aaa263eb1d2a02ee0010ef53ac06bd23d2ebd1
abspen1 <abspencer2097@gmail.com>
2021-08-17 11:00:09 -0700

    adding ~ so the cloud commands can be called anywhere

--- aliases.zsh
+++ aliases.zsh
@@ -81,9 +81,9 @@ git reset HEAD~1 --hard
 
 # Adding Meteor shortcut since download not working properly on M1
 alias meteor='/usr/local/bin/launch-meteor'
-alias github='cd Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/GitHub/'
+alias github='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/GitHub/'
 
 # Adding various shortcuts to cloud docs
-alias cloud='cd Library/Mobile\ Documents/com\~apple\~CloudDocs/'
-alias school='cd Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/SCHOOL'
-alias work='cd Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/WORK'
+alias cloud='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/'
+alias school='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/SCHOOL'
+alias work='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/WORK'
