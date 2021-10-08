e443f762f3a74c7c70f6f8cd2f7ff7c3a894f79c
abspen1 <abspencer2097@gmail.com>
2021-10-07 18:25:06 -0700

    fix meteor call

--- aliases.zsh
+++ aliases.zsh
@@ -89,8 +89,7 @@ git reset HEAD~1 --hard
 # alias bdone = "!f() { DEFAULT=$(def); git checkout ${1-$DEFAULT} && git up && git bclean ${1-$DEFAULT}; }; f"
 
 # Adding Meteor shortcut since download not working properly on M1
-alias run-meteor='/usr/local/bin/launch-meteor run'
-alias cmd-meteor='/usr/local/bin/launch-meteor'
+alias meteor='/usr/local/bin/launch-meteor'
 alias github='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/GitHub/'
 
 # Adding various shortcuts to cloud docs
