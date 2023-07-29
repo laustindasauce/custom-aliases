26d451fe6b67ce88b768f37de9c9c3ca4fbd50f0
Austin Spencer <abspencer2097@gmail.com>
2023-07-29 08:11:53 -0700

    adding to git flow aliases

--- aliases.zsh
+++ aliases.zsh
@@ -32,20 +32,18 @@ alias gfff='echo -n "Feature name: " && read name && git flow feature finish $na
 git push'
 
 alias gfrs='echo -n "Release name: " && read name && git flow release start $name'
-alias gfrf='echo -n "Release name: " && read name && git flow release finish $name
-co main
-git push
-co develop
-git push
-git push --tags'
+alias gfrf='echo -n "Release name: " && read name && git flow release finish $name'
 
 alias gfhs='echo -n "Hotfix name: " && read name && git flow hotfix start $name'
-alias gfhf='echo -n "Hotfix name: " && read name && git flow hotfix finish $name
+alias gfhf='echo -n "Hotfix name: " && read name && git flow hotfix finish $name'
+
+alias gfp='
 co main
 git push
 co develop
 git push
-git push --tags'
+git push --tags
+'
 
 
 ### Git aliases
